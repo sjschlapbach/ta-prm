3676f4ff8cecdc7a9873d2528e459ac6181db869
sjschlapbach <julius.schlapbach@gmx.net>
2023-12-22 20:43:54 +0100

    feat: add possibility to save and load point objects to json

--- src/obstacles/point.py
+++ src/obstacles/point.py
@@ -1,4 +1,5 @@
 from shapely.geometry import Point as ShapelyPoint, LineString, Polygon
+from shapely import wkt
 from pandas import Interval
 from matplotlib.patches import Circle
 from typing import Union
@@ -114,3 +115,34 @@ class Point(Geometry):
             plt.plot(*poly.exterior.xy, color="black")
         else:
             plt.plot(*self.geometry.xy, color="black", marker="o")
+
+    def export_to_json(self):
+        """
+        Returns a JSON representation of the point object, using the corresponding exporting
+        function of the Geometry class and the stringified version of the geometry.
+
+        Returns:
+            str: A JSON representation of the point object.
+        """
+        if self.geometry is None:
+            return {**super().export_to_json(), "geometry": "None"}
+
+        return {**super().export_to_json(), "geometry": self.geometry.wkt}
+
+    def load_from_json(self, json_object):
+        """
+        Loads the point object from a JSON representation, using the corresponding loading
+        function of the Geometry class and the stringified version of the geometry.
+
+        Args:
+            json_object (dict): The JSON representation of the point object.
+
+        Returns:
+            Object with updated attributes for both the geometry and the parent Geometry object
+        """
+        super().load_from_json(json_object)
+
+        if json_object["geometry"] == "None":
+            self.geometry = None
+        else:
+            self.geometry = wkt.loads(json_object["geometry"])
