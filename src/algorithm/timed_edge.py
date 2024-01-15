2724244643823e6f89cb0a02b1401a927423dba6
sjschlapbach <julius.schlapbach@gmx.net>
2024-01-15 23:13:22 +0100

    feat: implement function to test if edge is available during interval

--- src/algorithm/timed_edge.py
+++ src/algorithm/timed_edge.py
@@ -35,3 +35,61 @@ class TimedEdge:
         self.geometry = geometry
         self.always_available = always_available
         self.availability = availability
+
+    def is_available(self, query_interval: Interval) -> bool:
+        """
+        Check if the edge is available for the given time interval.
+
+        Args:
+            query_interval (Interval): The time interval to check.
+
+        Returns:
+            bool: True if the edge is available, False otherwise.
+        """
+        if self.always_available:
+            return True
+
+        if len(self.availability) == 1:
+            return self.__covers_interval(self.availability[0], query_interval)
+
+        # perform binary search to find the first interval intersecting with the query_interval
+        left = 0
+        right = len(self.availability) - 1
+
+        while left <= right:
+            # only one interval is left from the search that potentially covers the query_interval
+            if left == right:
+                return self.__covers_interval(self.availability[left], query_interval)
+
+            # select the center interval
+            mid = (left + right) // 2
+
+            # if the query interval ends before the middle interval starts, search left
+            if self.availability[mid].lower > query_interval.upper:
+                right = mid - 1
+                continue
+
+            # if the query interval starts after the current interval ends, search right
+            elif self.availability[mid].upper < query_interval.lower:
+                left = mid + 1
+                continue
+
+            # query interval ends after the current interval starts and starts before the current interval ends
+            # -> the query interval at least partially lies within the current interval
+            else:
+                return self.__covers_interval(self.availability[mid], query_interval)
+
+        return False
+
+    def __covers_interval(self, interval: Interval, other: Interval) -> bool:
+        """
+        Check if the edge covers the given interval.
+
+        Args:
+            interval (Interval): The interval to check.
+            other (Interval): The interval that should be covered.
+
+        Returns:
+            bool: True if the edge covers the interval, False otherwise.
+        """
+        return interval.lower <= other.lower and interval.upper >= other.upper
