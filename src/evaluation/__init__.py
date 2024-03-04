3a683c955de7b54b72e3a4e69d2b1450fad07da7
sjschlapbach <julius.schlapbach@gmx.net>
2024-03-05 00:12:39 +0100

    benchmarking: add benchmark with different pruning parameters

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -1,5 +1,10 @@
 from .sample_benchmark import sample_benchmark
 from .obstacle_benchmark import obstacle_benchmark
+from .pruning_benchmark import (
+    pruning_benchmark,
+    print_pruning_analytics,
+    aggregate_pruning_benchmark_results,
+)
 from .helpers import (
     create_environment,
     run_algorithms,
